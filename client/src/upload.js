131a105b7dda330522259564962fef22dff6f5cb
iYung <ivankhyung@gmail.com>
2017-08-30 15:24:25 +0000

    set up upload form

--- client/src/upload.js
+++ client/src/upload.js
@@ -5,29 +5,12 @@ import {
   Grid,
   Button,
   Icon,
-  Image
+  Image,
+  List,
+  Input
 } from 'semantic-ui-react'
 
-class Upload extends Component {
-  
-  onChange () {
-    var f = document.getElementById("input").files[0];
-    var r = new FileReader();
-    r.onload = function(e) {
-      try {
-        var data = JSON.parse(JSON.parse(r.result)["m_teamInfo_Serialized"]);
-        alert(data["TeamPlayerInfo"]);
-      } catch (error) {
-        alert("Invalid file was uploaded!");
-      }
-    }
-    r.readAsText(f);
-  }
-  
-  uploadButtonPress () {
-    document.getElementById("input").click();
-  }
-  
+class DefaultPage extends Component {
   render() {
     return (
       <div>
@@ -37,8 +20,8 @@ class Upload extends Component {
             content='Upload a New Replay'
             style={{ fontSize: '3em', fontWeight: 'normal', textAlign: "center", paddingBottom: '.5em' }}
         />
-          <input type="file" id="input" style={{ textAlign: "center", display: "none"}} onChange={this.onChange}/>
-          <Button as='a' primary size='large' onClick={this.uploadButtonPress}>
+          <input type="file" id="input" style={{ textAlign: "center", display: "none"}} onChange={this.props.onChange}/>
+          <Button as='a' primary size='large' onClick={this.props.uploadButtonPress}>
           Select an .arr file
           <Icon name='right arrow' />
           </Button>
@@ -65,6 +48,164 @@ class Upload extends Component {
         </Segment>
       </div>
     );
+  }
+}
+
+class UploadForm extends Component {
+  
+  copyToClipboard() {
+    document.getElementById("sharingLink").select();
+    document.execCommand('copy');
+  }
+  
+  render() {
+    return (
+      <div>
+      <Segment inverted basic style={{ margin: "0 auto",  textAlign: "center", paddingBottom: '2em' }}>
+        <Input type='text' placeholder="Name your replay here" id="replayName" style={{ fontSize: '3em', fontWeight: 'normal', textAlign: "center", paddingBottom: '.5em' }}>
+        </Input>
+        <br/>
+        <Button as='a' primary size='large' download="file.arr" href={'data:text/plain;charset=utf-8,' + encodeURIComponent("HI")}>
+        Upload Replay
+        </Button>
+      </Segment>
+       <Segment style={{ padding: '5em 0em' }} vertical>
+          <Grid container stackable verticalAlign='top'>
+          <Grid.Row>
+              <Grid.Column width={5}>
+                <Header as='h3' style={{ fontSize: '2em' }}>Team 1</Header>
+                <List>
+                  <List.Item>
+                    <Image avatar src={require('./images/blackburn.png')} />
+                    <List.Content>
+                      <List.Header>Blackburn</List.Header>
+                      <List.Description>disasterPony#0214</List.Description>
+                    </List.Content>
+                  </List.Item>
+                  <List.Item>
+                    <Image avatar src={require('./images/blackburn.png')} />
+                    <List.Content>
+                      <List.Header>Blackburn</List.Header>
+                      <List.Description>disasterPony#0214</List.Description>
+                    </List.Content>
+                  </List.Item>
+                  <List.Item>
+                    <Image avatar src={require('./images/blackburn.png')} />
+                    <List.Content>
+                      <List.Header>Blackburn</List.Header>
+                      <List.Description>disasterPony#0214</List.Description>
+                    </List.Content>
+                  </List.Item>
+                  <List.Item>
+                    <Image avatar src={require('./images/blackburn.png')} />
+                    <List.Content>
+                      <List.Header>Blackburn</List.Header>
+                      <List.Description>disasterPony#0214</List.Description>
+                    </List.Content>
+                  </List.Item>
+                  <List.Item>
+                    <Image avatar src={require('./images/blackburn.png')} />
+                    <List.Content>
+                      <List.Header>Blackburn</List.Header>
+                      <List.Description>disasterPony#0214</List.Description>
+                    </List.Content>
+                  </List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={5}>
+                <Header as='h3' style={{ fontSize: '2em' }}>Team 2</Header>
+                <List>
+                  <List.Item>
+                    <Image avatar src={require('./images/blackburn.png')} />
+                    <List.Content>
+                      <List.Header>Blackburn</List.Header>
+                      <List.Description>disasterPony#0214</List.Description>
+                    </List.Content>
+                  </List.Item>
+                  <List.Item>
+                    <Image avatar src={require('./images/blackburn.png')} />
+                    <List.Content>
+                      <List.Header>Blackburn</List.Header>
+                      <List.Description>disasterPony#0214</List.Description>
+                    </List.Content>
+                  </List.Item>
+                  <List.Item>
+                    <Image avatar src={require('./images/blackburn.png')} />
+                    <List.Content>
+                      <List.Header>Blackburn</List.Header>
+                      <List.Description>disasterPony#0214</List.Description>
+                    </List.Content>
+                  </List.Item>
+                  <List.Item>
+                    <Image avatar src={require('./images/blackburn.png')} />
+                    <List.Content>
+                      <List.Header>Blackburn</List.Header>
+                      <List.Description>disasterPony#0214</List.Description>
+                    </List.Content>
+                  </List.Item>
+                  <List.Item>
+                    <Image avatar src={require('./images/blackburn.png')} />
+                    <List.Content>
+                      <List.Header>Blackburn</List.Header>
+                      <List.Description>disasterPony#0214</List.Description>
+                    </List.Content>
+                  </List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={6}>
+                <Header as='h3' style={{ fontSize: '2em' }}>Game Map</Header>
+                <p style={{ fontSize: '1.33em' }}>
+                  Reactor_Deathmatch
+                </p>
+                <Header as='h3' style={{ fontSize: '2em' }}>Date Uploaded</Header>
+                <p style={{ fontSize: '1.33em' }}>
+                  May 5, 2017
+                </p>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+      </div>
+    );
+  }
+}
+
+
+class Upload extends Component {
+  
+  onChange = () => {
+    var f = document.getElementById("input").files[0];
+    var r = new FileReader();
+    var self = this;
+    r.onload = function(e) {
+      try {
+        var data = JSON.parse(JSON.parse(r.result)["m_teamInfo_Serialized"]);
+        alert(data["TeamPlayerInfo"]);
+        self.setState({fileChosen: true});
+      } catch (error) {
+        alert("Invalid file was uploaded!");
+      }
+    }
+    r.readAsText(f);
+  }
+  
+  uploadButtonPress () {
+    document.getElementById("input").click();
+  }
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+        fileChosen: false
+    };
+  }
+  
+  render() {
+    if (this.state.fileChosen) {
+      return (<UploadForm />);
+    } else {
+      return (<DefaultPage onChange={this.onChange} uploadButtonPress={this.uploadButtonPress}/>);
+    }
   }
 }
 
