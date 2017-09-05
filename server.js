66d6d6857c8284fea6ee4e80617ce5ffe65d79cd
iYung <ivankhyung@gmail.com>
2017-09-05 04:14:28 +0000

    server setup

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
-import config from './config'
+var Replay = require('./schemas/replay');
+
+var config = require('./config');
 
 //db connect
-mongoose.connect(config.database);
+mongoose.connect(config.database,{useMongoClient: true});
 
 //port setup
 var port;
@@ -16,3 +18,39 @@ if (process.env.NODE_ENV === 'production') {
     port = config.serverPort;
 }
 
+//allow CORS
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Coantrol-Allow-Headers', 'Content-Type');
+  console.log('Req made');
+  next();
+});
+
+var router = express.Router();
+
+//get lastest replays, new replay
+router.route('/replay')
+    .get(function(req, res) {
+        Replay.find({
+            __v: 0
+        },function(err, replays) {
+            if (err)
+                return res.send(err);
+            return res.json(replays);
+        }).limit(15);
+    })
+    .post(function(req, res) {
+    });
+    
+//get replay
+router.route('/replay/:name')
+    .get(function(req, res) {
+    });
+
+//use router
+app.use('/api', router);
+app.listen(port);
+console.log('Magic happens on port ' + port);
